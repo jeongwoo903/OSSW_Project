816c6a1e10c9e53c42f2ac600eab1ba22ab7f083
kisak03 <kisak0386@gmail.com>
2022-11-09 16:53:33 +0900

    Fix: html run 오류 수정

--- project_TS/project_TS/settings.py
+++ project_TS/project_TS/settings.py
@@ -36,8 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles'
-    'app_TS.apps.App_TSConfig',
+    'django.contrib.staticfiles',
+    'app_TS.apps.AppTsConfig',
 ]
 
 MIDDLEWARE = [
