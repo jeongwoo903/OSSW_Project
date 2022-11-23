a704ddd79444536dd6098bc59db4cf12a62abb83
kisak03 <kisak0386@gmail.com>
2022-11-23 18:37:40 +0900

    Feat: 백엔드 최적화 작업 진행

--- project_TS/app_TS/views.py
+++ project_TS/app_TS/views.py
@@ -23,12 +23,13 @@ def translate(input_data):
     input_text = input_data.get('input_text')
     mid_language = input_data.get('language')
     language_list = {'EN': 'en', 'JP': 'ja', 'CH': 'zh-cn'}
+
     translator = Translator()
     text_mid = translator.translate(input_text, dest=language_list[mid_language])
     text_last = translator.translate(text_mid.text, dest=text_mid.src)
 
     return {
-        'text_first': {'lang': text_mid.src, 'text': text_mid.origin},
-        'text_mid': {'lang': text_mid.dest, 'text': text_mid.text},
-        'text_last': {'lang': text_last.dest, 'text': text_last.text}
+        'text_first': {'text': input_text},
+        'text_mid': {'text': text_mid.text},
+        'text_last': {'text': text_last.text}
     }
