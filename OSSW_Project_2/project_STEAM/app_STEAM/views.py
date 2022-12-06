892e0aa2f768b1eddb616ac1549025c6395e4761
kisak03 <kisak0386@gmail.com>
2022-12-06 16:23:31 +0900

    Comment: Add steam, steamspy api reference url

--- OSSW_Project_2/project_STEAM/app_STEAM/views.py
+++ OSSW_Project_2/project_STEAM/app_STEAM/views.py
@@ -14,11 +14,13 @@ def process_game_information():
     discount_game_list = []
     json_steamspy = requests_cache.CachedSession('cache_json_steamspy')
     json_steam = requests_cache.CachedSession('cache_json_steam')
+    # steamspy api reference : http://steamspy.com/api.php
     datas_steamspy_raw = json_steamspy.get("https://steamspy.com/api.php?request=top100in2weeks")
     datas_steamspy = json.loads(datas_steamspy_raw.text)
 
     for i in datas_steamspy:
         if int(datas_steamspy[i]['discount']) != 0:
+            # steam api reference : https://stackoverflow.com/questions/46330864/steam-api-all-games
             datas_steam_raw = json_steam.get("https://store.steampowered.com/api/appdetails?appids=" + i)
             datas_steam = json.loads(datas_steam_raw.text)
             data = datas_steam[i]['data']
